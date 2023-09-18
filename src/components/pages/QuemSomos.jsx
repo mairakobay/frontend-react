14061215ea3e0fe9ad9609f75394d39952200d84
Maira Kobayashi <mairakobay@gmail.com>
2023-09-18 17:58:07 -0300

    Criado CardUser

--- src/components/pages/QuemSomos.jsx
+++ src/components/pages/QuemSomos.jsx
@@ -2,7 +2,19 @@ import Header from "../layout/Header"
 import Sidebar from "../layout/Sidebar"
 import Content from "../layout/Content"
 import Footer from "../layout/Footer"
+import CardUser from "../ui/CardUser"
 
+const users = [{
+    nome: "Maira Kpbayashi",
+    email: "mairakobay@gmail.com",
+    photo: "https://avatars.githubusercontent.com/u/130098548?v=4"
+},
+{
+    nome: "Maira Kpbayashi",
+    email: "mairakobay@gmail.com",
+    photo: "https://avatars.githubusercontent.com/u/130098548?v=4"
+}
+]
 
 const QuemSomos = () => {
     return (
@@ -11,7 +23,17 @@ const QuemSomos = () => {
             <div id="main">
                 <Sidebar />  
                 <Content>
-                    <h1>Quem Somos</h1>
+                    <h1>Quem Somos</h1> 
+                    {
+                        users.map((user) => {
+                            <CardUser /> 
+
+                        }
+                        )
+                    }                 
+                    <CardUser user={user}/>
+                      
+                    <CardUser />     
                 </Content>      
             </div>
             <Footer />
